1f5ca483d7c89c1d690f52bd50f998f62c2ac8d6
Ngo Dinh Nam <ngodinhnam.dev@gmail.com>
2025-03-28 15:09:35 +0700

    lan 3

--- core/App.php
+++ core/App.php
@@ -1,10 +1,13 @@
 <?php
 
+require_once 'core/Request.php'; // Include class Request
+
 class App {
     protected $controller = 'HomeController';
     protected $action = 'index';
     protected $params = [];
-    protected $controllerName; // Thêm biến để lưu tên controller dưới dạng chuỗi
+    protected $controllerName; // Lưu tên controller dưới dạng chuỗi
+    protected $request; // Lưu đối tượng Request
 
     public function __construct() {
         $url = $this->parseUrl();
@@ -42,6 +45,9 @@ class App {
         // Params
         $this->params = array_values($url);
 
+        // Tạo đối tượng Request
+        $this->request = new Request($this->controllerName, $this->action, $this->params);
+
         // Áp dụng middleware trước khi gọi controller::action
         $this->applyMiddleware();
 
@@ -64,15 +70,21 @@ class App {
 
     protected function applyMiddleware() {
         $middlewares = require_once 'config/middlewares.php';
-        // Sử dụng $this->controllerName thay vì $this->controller
-        $currentRoute = strtolower(str_replace('Controller', '', $this->controllerName)) . '/' . $this->action;
+        // Sử dụng $this->request->getRoute() để lấy route hiện tại
+        $currentRoute = $this->request->getRoute();
     
         if (isset($middlewares[$currentRoute])) {
             foreach ($middlewares[$currentRoute] as $middleware) {
                 list($class, $method) = explode('::', $middleware);
                 require_once 'middleware/' . $class . '.php';
-                call_user_func([$class, $method]);
+                // Truyền đối tượng Request vào middleware
+                call_user_func([$class, $method], $this->request);
             }
         }
+    }
+
+    // Getter để truy cập Request từ bên ngoài (nếu cần)
+    public function getRequest() {
+        return $this->request;
     }
 }
